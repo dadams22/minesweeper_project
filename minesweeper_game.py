0a260ef5e89181cabbfd3b2a1187554fe7b09088
David Adams <dadams2212@gmail.com>
2021-04-14 00:11:29 -0400

    Add _index method, height and width fields, and is_covered method to MinesweeperGame class

--- minesweeper_game.py
+++ minesweeper_game.py
@@ -2,5 +2,14 @@
 
 class MinesweeperGame:
     def __init__(self, bombs: str, width: int, height: int):
+        self.width = width
+        self.height = height
+
         self.bombs = [bomb == '1' for bomb in bombs]
         self.covered = [True] * (width * height)
+
+    def is_covered(self, x, y):
+        return self.covered[self._index(x, y)]
+
+    def _index(self, x: int, y: int):
+        return (y * self.height) + x
