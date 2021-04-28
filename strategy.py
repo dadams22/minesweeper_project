b80ab871fabe9db033ff92e9c1a4e59ad4902d1d
David Adams <dadams2212@gmail.com>
2021-04-27 23:37:45 -0400

    Get ProbabilityAlgorithm fully working

--- strategy.py
+++ strategy.py
@@ -14,22 +14,48 @@ class ProbabilityAlgorithm:
     def play_game(self):
         starting_square = self.game.get_starting_square()
         self.open_list.append(starting_square)
+
         while not self.game.game_over():
-            target_square = max(self.open_list, key=lambda coord: self._bomb_probability(*coord))
+            target_square = self._get_target_square()
+
+            if target_square is None:
+                covered_squares = self.game.get_covered_squares()
+                square_to_uncover = random.choice(covered_squares)
+                self._uncover_square(*square_to_uncover)
+                continue
+
             covered_neighbors = self.game.get_covered_neighbors(*target_square)
             if len(covered_neighbors) == self._get_effective_label(*target_square):
                 for neighbor in covered_neighbors:
-                    self.game.place_flag(*neighbor)
-                    self.update_discovered_bomb_counts(*neighbor)
+                    self._flag_square(*neighbor)
+
+                self.open_list.remove(target_square)
             else:
                 square_to_uncover = random.choice(covered_neighbors)
-                label = self.game.uncover(*square_to_uncover)
-                if label == self.game.BOMB_LABEL:
-                    self.update_discovered_bomb_counts(*square_to_uncover)
-                else:
-                    self.open_list.append(square_to_uncover)
+                self._uncover_square(*square_to_uncover)
 
-    def update_discovered_bomb_counts(self, bomb_x: int, bomb_y: int):
+                if len(covered_neighbors) == 1 and target_square in self.open_list:
+                    self.open_list.remove(target_square)
+
+    def _uncover_square(self, x: int, y: int):
+        label = self.game.uncover(x, y)
+        if label == self.game.BOMB_LABEL:
+            self._update_discovered_bomb_counts(x, y)
+        else:
+            self.open_list.append((x, y))
+
+    def _flag_square(self, x: int, y: int):
+        self.game.place_flag(x, y)
+        self._update_discovered_bomb_counts(x, y)
+
+    def _get_target_square(self):
+        if self.open_list:
+            target = max(self.open_list, key=lambda coord: self._bomb_probability(*coord))
+            if self._bomb_probability(*target) > 0:
+                return target
+        return None
+
+    def _update_discovered_bomb_counts(self, bomb_x: int, bomb_y: int):
         neighbors = self.game.get_neighbors(bomb_x, bomb_y)
         for neighbor in neighbors:
             self.discovered_neighboring_bombs[self._index(*neighbor)] += 1
