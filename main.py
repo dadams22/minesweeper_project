f5f4a1b89f163b7f508039859f963e582e51ebc1
David Adams <dadams2212@gmail.com>
2021-04-27 20:14:43 -0400

    Remove flag testing code

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ from minesweeper_game import MinesweeperGame
 if __name__ == '__main__':
     game = MinesweeperGame('110001110000111', 5, 3)
 
-    print(game.get_starting_square())
-
-    game.place_flag(0, 0)
-
-    game.place_flag(0, 3)
+    starting_square = game.get_starting_square()
+    print(starting_square)
 
     print(str(game))
