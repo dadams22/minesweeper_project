37d23621f3848a86fae2e5c28411cae8bebb1c9d
David Adams <dadams2212@gmail.com>
2021-04-27 23:12:36 -0400

    Add main logic to ProbabilityAlgorithm for play_game method

--- strategy.py
+++ strategy.py
@@ -7,19 +7,34 @@ class ProbabilityAlgorithm:
     def __init__(self, game: MinesweeperGame):
         self.game = game
 
-        self.queue = PriorityQueue()
+        self.open_list = []
 
         self.discovered_neighboring_bombs = [0] * (game.width * game.height)
 
     def play_game(self):
         starting_square = self.game.get_starting_square()
-        current_square = None
+        self.open_list.append(starting_square)
         while not self.game.game_over():
-            if current_square is None:
-                covered_squares = self.game.get_covered_squares()
-                self.game.uncover(*random.choice(covered_squares))
+            target_square = max(self.open_list, key=lambda coord: self._bomb_probability(*coord))
+            covered_neighbors = self.game.get_covered_neighbors(*target_square)
+            if len(covered_neighbors) == self._get_effective_label(*target_square):
+                for neighbor in covered_neighbors:
+                    self.game.place_flag(*neighbor)
+                    self.update_discovered_bomb_counts(*neighbor)
+            else:
+                square_to_uncover = random.choice(covered_neighbors)
+                label = self.game.uncover(*square_to_uncover)
+                if label == self.game.BOMB_LABEL:
+                    self.update_discovered_bomb_counts(*square_to_uncover)
+                else:
+                    self.open_list.append(square_to_uncover)
 
-    def get_effective_label(self, x, y):
+    def update_discovered_bomb_counts(self, bomb_x: int, bomb_y: int):
+        neighbors = self.game.get_neighbors(bomb_x, bomb_y)
+        for neighbor in neighbors:
+            self.discovered_neighboring_bombs[self._index(*neighbor)] += 1
+
+    def _get_effective_label(self, x: int, y: int) -> int:
         return self.game.get_label(x, y) - self.discovered_neighboring_bombs[self._index(x, y)]
 
     def _bomb_probability(self, x: int, y: int) -> float:
