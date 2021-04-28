4ce92faaf52422878e971773511bdcec8dbc8d6e
David Adams <dadams2212@gmail.com>
2021-04-28 00:19:02 -0400

    Modify the get_all_files function for readability

--- analysis.py
+++ analysis.py
@@ -3,4 +3,9 @@ from os.path import isfile, join
 
 
 def get_all_files(directory_path: str):
-    return [join(directory_path, f) for f in listdir(directory_path) if isfile(join(directory_path, f))]
+    files = []
+    for file in listdir(directory_path):
+        full_filepath = join(directory_path, file)
+        if isfile(full_filepath):
+            files.append(full_filepath)
+    return files
