bae58efb5a0415f8f952cd11c69169fa8d944d04
David Adams <dadams2212@gmail.com>
2021-04-27 20:59:41 -0400

    Add effective label calculation to ProbabilityAlgorithm

--- strategy.py
+++ strategy.py
@@ -7,3 +7,11 @@ class ProbabilityAlgorithm:
         self.game = game
 
         self.queue = PriorityQueue()
+
+        self.discovered_neighboring_bombs = [0] * (self.width * self.height)
+
+    def get_effective_label(self, x, y):
+        return self.game.get_label(x, y) - self.discovered_neighboring_bombs[self._index(x, y)]
+
+    def _index(self, x: int, y: int):
+        return (y * self.game.width) + x
