ee7d0b419788a5eb6e9d0a59b5a3e911a031f0d8
David Adams <dadams2212@gmail.com>
2021-04-27 23:11:18 -0400

    Add method to ProbabilityAlgorithm that roughly estimates the probability that a bomb resides in one of its covered neighbors.

--- strategy.py
+++ strategy.py
@@ -22,5 +22,14 @@ class ProbabilityAlgorithm:
     def get_effective_label(self, x, y):
         return self.game.get_label(x, y) - self.discovered_neighboring_bombs[self._index(x, y)]
 
+    def _bomb_probability(self, x: int, y: int) -> float:
+        covered_neighbors = len(self.game.get_covered_neighbors(x, y))
+        effective_label = self._get_effective_label(x, y)
+
+        if effective_label == 0 or covered_neighbors == 0:
+            return 0
+
+        return effective_label / covered_neighbors
+
     def _index(self, x: int, y: int):
         return (y * self.game.width) + x
