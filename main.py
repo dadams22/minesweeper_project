027ddba2c44b8d5961f54f177e8df6343f015177
David Adams <dadams2212@gmail.com>
2021-04-28 01:53:17 -0400

    Add print statements to indicate testing progress in main.py

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ from analysis import bomb_density_test, board_size_test
 
 
 if __name__ == '__main__':
+    print('Beginning Density Test')
     bomb_density_test(ProbabilityAlgorithm)
+    print('Density Test Complete')
+
+    print('Beginning Size Test')
     board_size_test(ProbabilityAlgorithm)
+    print('Size Test Complete')
