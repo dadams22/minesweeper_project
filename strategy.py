dd99b2de7d60c2d31bdd1ed81265f51ff5f8a223
David Adams <dadams2212@gmail.com>
2021-04-27 20:27:05 -0400

    Continue to develop ProbabilityAlgorithm

--- strategy.py
+++ strategy.py
@@ -1,3 +1,9 @@
+from .minesweeper_game import MinesweeperGame
+from queue import PriorityQueue
+
+
 class ProbabilityAlgorithm:
-    def __init__(self):
-        pass
+    def __init__(self, game: MinesweeperGame):
+        self.game = game
+
+        self.queue = PriorityQueue()
